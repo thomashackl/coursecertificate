28d12701fd9bfe9bc8e9cac886327dfd1047530b
Thomas Hackl <thomas.hackl@uni-passau.de>
2015-02-02 09:04:37 +0100

    ignore "Courses in free semester time" sem tree area

--- trails/templates/jura.php
+++ trails/templates/jura.php
@@ -5,7 +5,7 @@ class certificate_jura extends certificate {
     public $name = 'Schlüsselkompetenzen Jura';
     public $beschreibung = "Das Zentrum für Schlüsselkompetenzen der Universität Passau ist eine zentrale wissenschaftliche Einrichtung, die als Ergänzung zum akademischen Fachstudium Veranstaltungen aus dem Bereich überfachlicher Kompetenzen anbietet. Studierende, die das größtenteils freiwillige Veranstaltungsangebot in Anspruch nehmen, beweisen damit Eigeninitiative und eine hohe Motivation zur persönlichen Weiterentwicklung.";
     public $sem_tree_id = '23bd2f0b9f437b60729290733961853d';
-    public $exclude_sem_tree_ids = array();
+    public $exclude_sem_tree_ids = array('c0b4af8e91ef5022141ec58f17e69b21');
 
     public function export($seminars) {
         $this->loadSeminarsForPDF();

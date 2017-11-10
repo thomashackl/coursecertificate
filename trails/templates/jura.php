eea0832a821b39ced403eb3566fe79dbe4fee095
hackl <thomas.hackl@uni-passau.de>
2017-11-10 09:53:40 +0100

    jura cert just like former zfs cert

--- trails/templates/jura.php
+++ trails/templates/jura.php
@@ -5,10 +5,30 @@ class certificate_jura extends certificate {
     public $order = 2;
 
     public $name = 'Schlüsselkompetenzen Jura';
-    public $beschreibung = "Das Zentrum für Schlüsselkompetenzen der Universität Passau ist eine zentrale wissenschaftliche Einrichtung, die als Ergänzung zum akademischen Fachstudium Veranstaltungen aus dem Bereich überfachlicher Kompetenzen anbietet. Studierende, die das größtenteils freiwillige Veranstaltungsangebot in Anspruch nehmen, beweisen damit Eigeninitiative und eine hohe Motivation zur persönlichen Weiterentwicklung.";
+    public $beschreibung = "Das Zentrum für Karriere und Kompetenzen (ZKK) bietet als Ergänzung zum akademischen Fachstudium Seminare und IT-Kurse zum Erwerb überfachlicher Kompetenzen an. Durch diese Kompetenzen werden Reflexivität und Verantwortungsbewusstsein gestärkt sowie selbstorganisiertes und eigenständiges Lernen vermittelt und gefördert. Studierende, die in allen drei Kompetenzbereichen Seminare besucht haben, beweisen damit Eigeninitiative und eine hohe Motivation zur persönlichen Weiterentwicklung.";
     public $sem_tree_id = '23bd2f0b9f437b60729290733961853d';
 
-    public $exclude_sem_tree_ids = array('c0b4af8e91ef5022141ec58f17e69b21');
+    public $exclude_sem_tree_ids = array(
+        'c0b4af8e91ef5022141ec58f17e69b21',
+        '814e28f539e0952dfe1f35bca8800bee'
+    );
+    public $invisible_sem_tree_ids = array(
+        'c0b4af8e91ef5022141ec58f17e69b21',
+        '814e28f539e0952dfe1f35bca8800bee',
+        '30906bf846b53c8a9e2fe21d2180f818',
+        '6a29bdcc362a3c2f5ea09efda19c5e49',
+        '42e7d7a3481c706ed1d54dc072d552e0',
+        '701cff7eda6876be28da2fcd62ded48a',
+        '0b30d769e0dcf80fda308272b87814ca',
+        '7df63ae06d54e6c6ed7dcd48b80259a0',
+        '9d88ab9caba20e819765c4b5a96b63b2',
+        '855037ff174597580d4ba3f1267a9328',
+        '304aa7e5e378dc260ffa596d7df50559',
+        '04f08c9ca84f098d236d2befd6da7940',
+        '2b86bdce5d33e98ac4f205e841c2354b',
+        '0db0a0bb3aab3cd62dcd8c17fa20b312'
+    );
+
     public function export($seminars) {
         $this->loadSeminarsForPDF();
         // Create certificate.
@@ -27,7 +47,7 @@ class certificate_jura extends certificate {
         // Set to German locale.
         setlocale(LC_TIME, "de_DE");
         $pdf->SetFont('Arial', '', 11);
-        $pdf->MultiCell(180, 5, legacy_studip_utf8decode("hat am Zentrum für Schlüsselkompetenzen der " .
+        $pdf->MultiCell(180, 5, legacy_studip_utf8decode("hat am Zentrum für Karriere und Kompetenzen der " .
                 "Universität Passau im Zeitraum von " .
                 strftime('%B %Y', $this->start) .
                 " bis " . strftime('%B %Y', $this->end) . " an " . $this->getCount() .
